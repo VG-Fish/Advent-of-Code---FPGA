f7380ba97d62970b3f454fc39c66a0eabb587ca3
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2026-06-17 14:47:20 -0500

    improved solution

--- Problem 1/main.py
+++ Problem 1/main.py
@@ -1,13 +1,11 @@
-direction_sign: dict[str, int] = {"L": -1, "R": 1}
-
 def main():
     current: int = 50
     res: int = 0
     with open("input.txt", "r") as f:
         for line in f:
-            direction: str = line[0]
+            direction: int = -1 if line[0] == "L" else 1
             distance: int = int(line[1:])
-            current = (direction_sign[direction] * distance + current) % 100
+            current = (direction * distance + current) % 100
             if current == 0:
                 res += 1
     print(res)
